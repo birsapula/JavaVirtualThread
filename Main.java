55872889da656b923c7f9524a67d921c679526d3
FreedomToCode <100515586+birsapula@users.noreply.github.com>
2023-08-13 16:31:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.sql.SQLOutput;
 import java.util.concurrent.ThreadFactory;
 
@@ -8,27 +10,50 @@ import static java.lang.Thread.currentThread;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
     public static void doWork() {
+
         try {
-            Thread.sleep(5000);
+            Thread.sleep(100000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args)  {
-        int MAX = 100_0000;
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println("Available CPU cores: " + availableProcessors);
+        int MAX = 100_000;
         for (int i = 0; i < MAX; i++) {
             //new Thread(Main::doWork).start();
-           Thread.startVirtualThread(Main::doWork);
+            //Thread.ofVirtual().start(Main::doWork);
+            Thread.ofPlatform().start(Main::doWork);
+            //System.out.println(">> i -- "+i%500);
+            if(i%500==0){
+            Thread.ofVirtual().start(Main::checkCPUInfo);
+            }
         }
-        System.out.println("DOne");
+
+        System.out.println("Wiat for all Done");
         try {
-            Thread.sleep(10000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
+        System.out.println("DONE");
 
+    }
+public static void checkCPUInfo(){
+
+
+    // Get the number of live threads and peak thread count
+    int liveThreads = threadMXBean.getThreadCount();
+    int peakThreads = threadMXBean.getPeakThreadCount();
+
+    System.out.println("Number of live threads: " + liveThreads);
+    System.out.println("Peak thread count: " + peakThreads);
+
+
+}
 
 }
